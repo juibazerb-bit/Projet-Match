50c16010a0aaa6b3850fcffb2b25ab002fb37e86
juibazerb-bit <juib.azerb@gmail.com>
2026-05-11 18:32:56 +0200

    correction points bonus

--- OutilsProjets/src/Test/TestPlateauFenetreGraphique.java
+++ OutilsProjets/src/Test/TestPlateauFenetreGraphique.java
@@ -21,11 +21,11 @@ public class TestPlateauFenetreGraphique {
     public static void main(String[] args) {
         int nbLignes = 20;
         int nbCol = 20;
-        int nbTypes = 7;
+        int nbTypes = 5;
         int margeX = 100;
         int margeY = 100;
 
-        Plateau plateau = new Plateau(nbLignes, nbCol, nbTypes, 42); // 42 = graine fixe
+        Plateau plateau = new Plateau(nbLignes, nbCol, nbTypes, 0); // 42 = graine fixe
         GestionIA ia = new GestionIA();
         int largeur = nbCol * Tuile.TAILLE + 300;
         int hauteur = nbLignes * Tuile.TAILLE + 300;
