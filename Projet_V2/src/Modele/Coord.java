5ba40041c4ef381a30bbe6ad98713c05f0656c3e
juibazerb-bit <juib.azerb@gmail.com>
2026-06-02 17:43:01 +0200

    v2

---
+++ Projet_V2/src/Modele/Coord.java
@@ -0,0 +1,59 @@
+package Modele;
+
+/**
+ * Représente une position (colonne, ligne) sur le plateau. abscisse = colonne,
+ * ordonnee = ligne.
+ */
+public class Coord {
+
+    private int abscisse;
+    private int ordonnee;
+
+    public Coord(int abscisse, int ordonnee) {
+        this.abscisse = abscisse;
+        this.ordonnee = ordonnee;
+    }
+
+    public int getAbscisse() {
+        return abscisse;
+    }
+
+    public int getOrdonnee() {
+        return ordonnee;
+    }
+
+    public void setAbscisse(int abscisse) {
+        this.abscisse = abscisse;
+    }
+
+    public void setOrdonnee(int ordonnee) {
+        this.ordonnee = ordonnee;
+    }
+
+    /**
+     * Deux cases sont voisines si elles ne diffèrent que d'une case en X ou en
+     * Y.
+     */
+    public boolean estVoisine(Coord c) {
+        int diffAbs = Math.abs(this.abscisse - c.abscisse);
+        int diffOrd = Math.abs(this.ordonnee - c.ordonnee);
+        return (diffAbs == 1 && diffOrd == 0) || (diffAbs == 0 && diffOrd == 1);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + abscisse + ", " + ordonnee + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Coord)) {
+            return false;
+        }
+        Coord other = (Coord) obj;
+        return this.abscisse == other.abscisse && this.ordonnee == other.ordonnee;
+    }
+}
---
+++ Projet_V2/src/Modele/Tuile.java
@@ -0,0 +1,83 @@
+package Modele;
+
+import java.awt.Graphics2D;
+import java.util.Random;
+
+/**
+ * Une tuile du plateau. Connaît son type (index dans TypeTuile) et sa position
+ * visuelle Y utilisée pendant les animations de chute.
+ *
+ * La méthode dessiner() prend un Graphics2D : la Tuile ne dépend plus de
+ * FenetreGraphique, ce qui supprime un couplage inutile.
+ */
+public class Tuile {
+
+    public static final int TAILLE = 50;
+
+    private int type;
+    private double posYVisuelle = -1; // -1 = pas encore d'animation en cours
+
+    // --- Constructeurs ---
+    /**
+     * Tuile avec type fixe.
+     */
+    public Tuile(int type) {
+        this.type = type;
+    }
+
+    /**
+     * Tuile avec type aléatoire parmi nbTypes valeurs.
+     */
+    public Tuile(int nbTypes, Random rand) {
+        this.type = rand.nextInt(nbTypes);
+    }
+
+    // --- Accesseurs ---
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public double getPosYVisuelle() {
+        return posYVisuelle;
+    }
+
+    public void setPosYVisuelle(double y) {
+        this.posYVisuelle = y;
+    }
+
+    // --- Affichage ---
+    /**
+     * Dessine la tuile à la position (x, y) de la grille. Si une animation est
+     * en cours (posYVisuelle != -1), utilise posYVisuelle comme ordonnée.
+     */
+    public void dessiner(int x, int y, Graphics2D g2) {
+        TypeTuile monType = TypeTuile.values()[type % TypeTuile.values().length];
+        int yFinal = (posYVisuelle == -1) ? y : (int) posYVisuelle;
+        g2.drawImage(monType.getImage(), x, yFinal, TAILLE, TAILLE, null);
+    }
+
+    /**
+     * Représentation console.
+     */
+    @Override
+    public String toString() {
+        TypeTuile[] valeurs = TypeTuile.values();
+        return valeurs[type % valeurs.length].afficher();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Tuile)) {
+            return false;
+        }
+        return this.type == ((Tuile) obj).type;
+    }
+
+}
