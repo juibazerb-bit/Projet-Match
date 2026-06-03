cb4cdada7516e65ff691219f8f89b7304f57f948
juibazerb-bit <juib.azerb@gmail.com>
2026-06-03 19:28:23 +0200

    aaaa

--- Projet_Match_V2/src/Jouer/JouerModeConsole.java
+++ Projet_Match_V2/src/Jouer/JouerModeConsole.java
@@ -15,7 +15,7 @@ public class JouerModeConsole {
     public static void main(String[] args) {
         int nbLignes = 10, nbCol = 10, nbTypes = 7;
 
-        Plateau plateau = new Plateau(nbLignes, nbCol, nbTypes);
+        Plateau plateau = new Plateau(nbLignes, nbCol, nbTypes,false);
         GestionIA ia = new GestionIA();
         GestionPartie gestionPartie = new GestionPartie();
 
